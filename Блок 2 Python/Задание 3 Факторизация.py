7cfb1818e216701aa60da28c612794e5c15655b6
mvlriaa <mvlriaa@gmail.com>
2026-02-20 15:39:03 +0900

    Update Задание 3 Факторизация.py

--- Блок 2 Python/Задание 3 Факторизация.py
+++ Блок 2 Python/Задание 3 Факторизация.py
@@ -10,5 +10,23 @@ def prime_factors(n):
     if n > 1:
         factors.append(n)
     return factors
-n = 56
+
+def prime_factor_optim(n):
+    factors = []
+    while n % 2 == 0:
+        factors.append(2)
+        n //= 2
+    d = 3
+    while d * d <= n:
+        while n % d == 0:
+            factors.append(d)
+            n //= d
+        d += 2
+    if n > 1:
+        factors.append(n)
+
+    return factors
+
+n = 987654321987654321
 print(prime_factors(n))
+print(prime_factor_optim(n))
